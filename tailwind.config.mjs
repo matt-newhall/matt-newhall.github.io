0f87a495288ea0ddf8ccce00083d83eb0d9b22b9
matt-newhall <matt@newhall.dev>
2024-04-26 00:11:25 +0100

    refactor: converted css to tailwind styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'main': '#ede7de',
+			'main-dark': '#233831',
+			'primary': '#f61067',
+			'primary-dark': '#f61067',
+			'secondary': '#f4f0ea',
+			'secondary-dark': '#203b31',
+		},
+		fontFamily: {
+			'heading': ['GT', 'Menlo', 'Monaco'],
+			'sans': ['GT', 'Menlo', 'Monaco'],
+		}
 	},
 	plugins: [],
+	darkMode: 'selector',
 }
